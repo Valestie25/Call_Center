c3df938d268d24edbc7776a98677b5e8c35a844c
kevinxd3 <kevinramirezrodolfo@hotmail.com>
2022-07-30 10:50:40 -0700

    commit vista empleados

--- resources/views/layouts/vistadepartamentoA.blade.php
+++ resources/views/layouts/vistadepartamentoA.blade.php
@@ -15,60 +15,66 @@
     <link rel="stylesheet" href="{{ asset('css/highcharts.css')}}">
     <title>@yield('title', 'Documento')</title>
     <style>
-html {width: 100%;
-    height: 100%}
+html,div {width: 100%;
+    height: 100%;
+  }
+svg{
+  margin:0 auto; 
+}
 
 </style>
 </head>
 <body class="bg-withLight-400 w-full h-full">
-        <div>Vista dieno</div>
+        <div>Vista dieno 
         <svg version="1.1"
      baseProfile="full"
-     width="auto" height="auto"
+     width="100%" height="100%" 
      xmlns="http://www.w3.org/2000/svg">
   
-  <circle cx="150" cy="100" r="30" fill="blue" />
-  <circle cx="350" cy="100" r="30" fill="blue" />
-  <circle cx="550" cy="100" r="30" fill="blue" />
-  <circle cx="750" cy="100" r="30" fill="blue" />
+  <circle id="employee1" cx="150" cy="100" r="30" fill="blue" />
+  <circle id="employee2" cx="350" cy="100" r="30" fill="blue" />
+  <circle id="employee3" cx="550" cy="100" r="30" fill="blue" />
+  <circle id="employee4" cx="750" cy="100" r="30" fill="blue" />
   
   <style>
     rect {
      margin-top:100px;
     }
+    
   </style>
-  <rect x="60" y="140" width="800" height="100" style="padding-bottom:200px;" fill="grey" />
+  <rect x="60" y="140" width="800" height="100" style="padding-bottom:200px;" fill="lightgray" />
 
     
-  <circle cx="150" cy="300" r="30" fill="blue" />
-  <circle cx="350" cy="300" r="30" fill="blue" />
-  <circle cx="550" cy="300" r="30" fill="blue" />
-  <circle cx="750" cy="300" r="30" fill="blue" />
+  <circle id="employee5" cx="150" cy="300" r="30" fill="blue" />
+  <circle id="employee6" cx="350" cy="300" r="30" fill="blue" />
+  <circle id="employee7" cx="550" cy="300" r="30" fill="blue" />
+  <circle id="employee8" cx="750" cy="300" r="30" fill="blue" />
 
-  <rect x="60" y="350" width="800" height="100" style="padding-bottom:200px;" fill="grey" />
-
-  
-  <circle cx="150" cy="500" r="30" fill="blue" />
-  <circle cx="350" cy="500" r="30" fill="blue" />
-  <circle cx="550" cy="500" r="30" fill="blue" />
-  <circle cx="750" cy="500" r="30" fill="blue" />
-
-  <rect x="60" y="550" width="800" height="100" style="padding-bottom:200px;" fill="grey" />
+  <rect x="60" y="350" width="800" height="100" style="padding-bottom:200px;" fill="lightgray" />
 
   
-  <circle cx="150" cy="700" r="30" fill="blue" />
-  <circle cx="350" cy="700" r="30" fill="blue" />
-  <circle cx="550" cy="700" r="30" fill="blue" />
-  <circle cx="750" cy="700" r="30" fill="blue" />
+  <circle id="employee9" cx="150" cy="500" r="30" fill="blue" />
+  <circle id="employee10" cx="350" cy="500" r="30" fill="blue" />
+  <circle id="employee11" cx="550" cy="500" r="30" fill="blue" />
+  <circle id="employee12" cx="750" cy="500" r="30" fill="blue" />
 
-  <rect x="60" y="750" width="800" height="100" style="padding-bottom:200px;" fill="grey" />
+  <rect x="60" y="550" width="800" height="100" style="padding-bottom:200px;" fill="lightgray" />
+
+  
+  <circle id="employee13" cx="150" cy="700" r="30" fill="blue" />
+  <circle id="employee14" cx="350" cy="700" r="30" fill="blue" />
+  <circle id="employee15" cx="550" cy="700" r="30" fill="blue" />
+  <circle id="employee16" cx="750" cy="700" r="30" fill="blue" />
+
+  <rect x="60" y="750" width="800" height="100" style="padding-bottom:200px;" fill="lightgray" />
 </svg>
+</div>
  <!-- 
 <img
     src="/svg/departamento.svg"
     alt="diagrama del departamento"
     />
-   
+  !-->
 </body>
 
 </html>
