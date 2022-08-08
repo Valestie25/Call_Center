1ec6e5e3c90199661434bcc834220e0a69885ecc
Valestie25 <martinmarcosavila@gmail.com>
2022-08-08 13:28:34 -0700

    marcos designe

--- resources/views/layouts/departamentoD.blade.php
+++ resources/views/layouts/departamentoD.blade.php
@@ -5,34 +5,15 @@
 @section('content')
 
 <div>Departamento D</div>
-    <div class="container items-center w-full">
-        <div class="flex m-2">
-            <button class="text-base  rounded-r-none  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
-            hover:bg-gray-200  
-            bg-gray-100 
-            text-black-500 
-            border duration-200 ease-in-out 
-            border-gray-600 transition">
-                <div class="flex leading-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left w-5 h-5">
-                        <polyline points="15 18 9 12 15 6"></polyline>
-                    </svg>
-                    Back</div>
+<div class="text-center w-full">
+    <div class="inline-flex m-2">
+        <a class="flex " href="seccion1">
+            <button id="map" class="bg-white sticky duration-500 border-2 rounded-md border-black-400 hover:border-withLight-200 w-24 transform hover:translate-x-1 h-10 text-2xl hover:bg-black-400 hover:text-withLight-200 text-black-400">
+                <i class="fa-solid fa-map"></i>
             </button>
-            <button class="text-base  rounded-l-none  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
-            hover:bg-gray-200  
-            bg-gray-100 
-            text-black-500 
-            border duration-200 ease-in-out 
-            border-gray-600 transition">
-                <div class="flex leading-5">Next
-                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right w-5 h-5 ml-1">
-                        <polyline points="9 18 15 12 9 6"></polyline>
-                    </svg>
-                </div>
-            </button>
-        </div>
+        </a>
     </div>
+</div>
         <figure class=" highcharts-figure w-full"> 
             <div id="container1" class="border-4 border-sky-500 border-withLight-400 shadow-xl rounded"></div>
         </figure>
--- resources/views/layouts/vistadepartamentoA.blade.php
+++ resources/views/layouts/vistadepartamentoA.blade.php
@@ -4,6 +4,15 @@
 
 @section('content')
 <div class="static">Vista dieno
+  <div class="text-center w-full">
+    <div class="inline-flex m-2">
+        <a class="flex " href="departamentoA">
+          <button id="map" class="bg-white sticky duration-500 border-2 rounded-md border-black-400 hover:border-withLight-200 w-24 transform hover:translate-x-1 h-10 text-2xl hover:bg-black-400 hover:text-withLight-200 text-black-400">
+              <i class="fa-solid fa-chart-column"></i>
+            </button>
+        </a>
+    </div>
+</div>
 <div class = "grid grid-cols-4 gap-y-8">
   <div class="m-20">
     <a class="rounded-full px-14 py-12 bg-blueLight-600"></a>
